c3713183c2338465c8b658581a4f5674f8acea64
lobo-financiero <the_spellmaker@hotmail.com>
2025-05-22 11:10:20 -0600

    Update realLifeTest1.py

--- realLifeTest1.py
+++ realLifeTest1.py
@@ -17,7 +17,7 @@ api_key    = st.secrets["FMP_API_KEY"]
 st.set_page_config(page_title="Stock Portfolio Returns", layout="wide")
 
 # Function to fetch stock data from FMP API
-#@cache_data(ttl=43200)  # Cache for 12 hours
+@cache_data(ttl=43200)  # Cache for 12 hours
 def fetch_stock_data(symbols, api_key, start_date, end_date):
     data = {}
     for symbol in symbols:
